336549b4d0545af0bc5817429603de05b05ffed1
alirezahaqi <ali123haghi@gmail.com>
2022-07-27 13:19:13 +0430

    Update matrix_mul.v

--- matrix_mul.v
+++ matrix_mul.v
@@ -1,15 +1,10 @@
-module jdoodle;
-    initial begin
-        $display ("Welcome to JDoodle!!!");
-        $finish;
-    end
-endmodule
 
 module matrix_mul
-#(parameter n = 2);
-    reg [31 : 0] mat_A[n : 0][n : 0];
-    reg [31: 0] mat_B[n : 0][n : 0];
-    wire [31: 0] mat_out [n : 0][n : 0];
+#(parameter n = 2)
+(input [32 * n * n  : 0] mat_A, input [32 * n * n : 0] mat_B, output [32 * n * n : 0] mat_out);
+//(input [32 * n * n  : 0] mat_A, input [31: 0] mat_B[n : 0], output [31: 0] mat_out [n : 0][n : 0]);
+//input [32 * n * n  : 0] mat_A
+    
     wire [31: 0] mul_res [n : 0][n : 0][n : 0]; 
     wire [31: 0] acc_res [n : 0][n : 0][n : 0];
     genvar i, j, k, l;
@@ -20,8 +15,10 @@ module matrix_mul
             for (j = 0; j < n; j = j + 1)
                 for (k = 0; k < n; k = k + 1)
                 begin
-                    mul i_j_k(mat_A[i][j], mat_B[j][k], mul_res[i][k][j]);
+                    //mul i_j_k(mat_A[i][j], mat_B[j][k], mul_res[i][k][j]);
+                    mul i_j_k(mat_A[(i * n +  j) * 32 + 31 : (i * n +  j) * 32], mat_B[(j * n + k) * 32 + 31 : (j * n + k) * 32], mul_res[i][k][j]);
                 end
+        
         for (i = 0; i < n; i = i + 1)
             for (j = n - 1; j < n; j = j + 1)
                 for (k = 0; k < n; k = k + 1)
@@ -35,7 +32,8 @@ module matrix_mul
         for (i = 0; i < n; i = i + 1)
             for (j = n - 1; j < n; j = j + 1)
                 for (k = 0; k < n; k = k + 1)
-                    assign mat_out[i][k] = acc_res[i][k][0];
+                    //assign mat_out[i][k] = acc_res[i][k][0];
+                    assign mat_out[(i * n + k)*32 + 31 : (i * n + k)*32] = acc_res[i][k][0];
     endgenerate
 
 endmodule
@@ -44,5 +42,5 @@ module adder(input [31 : 0] a, input [31: 0] b, output [31: 0] c);
     assign c = a + b;
 endmodule
 module mul(input [31: 0] a, input [31: 0] b, output [31: 0] c);
-    assign c = a + b;
+    assign c = a * b;
 endmodule
